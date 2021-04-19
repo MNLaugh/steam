e154383ae6522340bea49bf37a14a45e198018fe
MNLaugh <contact@mnlaugh.com>
2021-04-19 02:56:01 +0200

    v1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,261 @@
-# steam
+# Deno steam api
+
+Deno Wrapper to communicate with Steam Web API
+
+## SteamID64 Finder
+
+Please refer to [http://steamid.co/](http://steamid.co/) or [http://steamidconverter.com/](http://steamidconverter.com/) to find the user steam id.
+
+## Usage
+
+```ts
+import Steam from "https://deno.land/x/steam/mod.ts";
+
+const steam = new Steam("your api key");
+```
+
+## Api
+
+- ### GetAppList
+
+  - @param format (Optional) [Format](###format)
+  - @return [Output](###output)
+
+    ```ts
+    /**
+     * Get all steam applications data.
+     * @param {Format?} format
+     * @return {Output}
+     */
+    const apps = await steam.GetAppList().catch(console.error);
+    ```
+
+- ### GetServersAtAddress
+
+  - @param addr [String](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String)
+  - @param format (Optional) [Format](###format)
+  - @return [Output](###output)
+
+    ```ts
+    /**
+     * Get all steam application data from a server.
+     * @param {String!} addr IP adress of target server.
+     * @param {Format?} format
+     * @return {Output}
+     */
+    const servers = await steam.GetServersAtAddress("ip").catch(console.error);
+    ```
+
+- ### GetNewsForApp
+
+  - @param appid [String](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String)
+  - @param options (Optional) [GetNewsForAppOptions](###GetNewsForAppOptions)
+  - @return [Output](###output)
+
+    ```ts
+    /**
+     * Returns the latest news of a game specified by its appID.
+     * @param appid
+     * @param {GetNewsForAppOptions} options
+     * @return {Output}
+     */
+    const news = await steam.GetNewsForApp(440, { count: 3 }).catch(console.error);
+    ```
+
+- ### GetGlobalAchievementPercentagesForApp
+
+  - @param gameid [Number](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number)
+  - @param format (Optional) [Format](###format)
+  - @return [Output](###output)
+
+    ```ts
+    /**
+     * Returns on global achievements overview of a specific game in percentages.
+     * @param {number!} gameid AppID of the game you want the news of.
+     * @param {Format?} format
+     * @return {Output}
+     */
+    const GlobalAchievementPercentages = await steam.GetGlobalAchievementPercentagesForApp({ gameid: 440 }).catch(console.error);
+    ```
+
+- ### GetPlayerSummaries
+
+  - @param steamids [[String]](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array#description)
+  - @param format (Optional) [Format](###format)
+  - @return [Output](###output)
+
+    ```ts
+    /**
+     * Returns basic profile information for a list of 64-bit Steam IDs.
+     * @param {[string]!} steamids Comma-delimited list of 64 bit Steam IDs to return profile information for. Up to 100 Steam IDs can be requested.
+     * @param {Format?} format
+     * @return {Output}
+     */
+    const PlayerSummaries = await steam.GetPlayerSummaries({ steamids: "steam ID" }).catch(console.error);
+    ```
+
+- ### GetFriends
+
+  - @param steamid [String](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String)
+  - @param relationship [RelationShip](###RelationShip)
+  - @param format (Optional) [Format](###format)
+  - @return [Output](###output)
+
+    ```ts
+        /**
+     * Returns the friend list of any Steam user, provided his Steam Community profile visibility is set to "Public".
+     * @param {string!} steamid 64 bit Steam ID to return friend list for.
+     * @param {RelationShip!} relationship Relationship filter. Possibles values: all, friend.
+     * @param {Format?} format
+     * @return {Output}
+     */
+    const Friends = await steam.GetFriendList({ steamid: "steam ID", relationship: "all" }).catch(console.error);
+    ```
+
+- ### GetPlayerAchievements
+
+  - @param steamid [String](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String)
+  - @param appid [Number](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number)
+  - @param options (Optional) [PlayerAchievementsOptions](###PlayerAchievementsOptions)
+  - @return [Output](###output)
+
+    ```ts
+    /**
+     * Returns a list of achievements for this user by app id.
+     * @param {string!} steamid 64 bit Steam ID to return friend list for.
+     * @param {number!} appid The ID for the game you're requesting.
+     * @param {PlayerAchievementsOptions} options
+     * @return {Output}
+     */
+    const PlayerAchievements = await steam.GetPlayerAchievements("steam ID", 730).catch(console.error);
+    ```
+
+- ### GetUserStatsForGame
+
+  - @param steamid [String](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String)
+  - @param appid [Number](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number)
+  - @param options (Optional) [UserStatsForGameOptions](###UserStatsForGameOptions)
+  - @return [Output](###output)
+
+    ```ts
+    /**
+     * Returns a list of achievements for this user by app id.
+     * @param {string!} steamid 64 bit Steam ID to return friend list for.
+     * @param {number!} appid The ID for the game you're requesting.
+     * @param {UserStatsForGameOptions} options
+     * @return {Output}
+     */
+    const UserStats = await steam.GetUserStatsForGame("steam ID", 730).catch(console.error);
+    ```
+
+- ### GetNumberOfCurrentPlayers
+
+  - @param appid [Number](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number)
+  - @param format (Optional) [Format](###Format)
+  - @return [Output](###output)
+
+    ```ts
+    /**
+     * Returns number of current players by app id.
+     * @param {number!} appid The ID for the game you're requesting.
+     * @param {Format} format
+     * @return {Output}
+     */
+    const NumberOfCurrentPlayers = await steam.GetNumberOfCurrentPlayers(730).catch(console.error);
+    ```
+
+- ### GetOwnedGames
+
+  - @param steamid [String](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String)
+  - @param options (Optional) [OwnedGamesOptions](###OwnedGamesOptions)
+  - @return [Output](###output)
+
+    ```ts
+    /**
+     * Returns a list of games a player owns along with some playtime information.
+     * @param {string!} steamid 64 bit Steam ID to return friend list for.
+     * @param {OwnedGamesOptions} options
+     * @return {Output}
+     */
+    const Games = await steam.GetOwnedGames("steam ID").catch(console.error);
+    ```
+
+- ### GetRecentlyPlayedGames
+
+  - @param steamid [String](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String)
+  - @param options (Optional) [RecentlyPlayedGamesOptions](###RecentlyPlayedGamesOptions)
+  - @return [Output](###output)
+
+    ```ts
+    /**
+     * Returns a list of games a player has played in the last two weeks.
+     * @param {string!} steamid 64 bit Steam ID to return friend list for.
+     * @param {RecentlyPlayedGamesOptions} options
+     * @return {Output}
+     */
+    const Games = await steam.GetRecentlyPlayedGames("steam ID").catch(console.error);
+    ```
+
+## TypeDef
+
+- ### Format
+
+    ```ts
+    type Format = "json" | "xml";
+    ```
+
+- ### RelationShip
+
+    ```ts
+    type RelationShip = "all" | "friend";
+    ```
+
+- ### GetNewsForAppOptions
+
+    ```ts
+    type GetNewsForAppOptions = {
+        count?: number,
+        maxlength?: number,
+        format?: Format
+    };
+    ```
+
+- ### PlayerAchievementsOptions
+
+    ```ts
+    type PlayerAchievementsOptions = {
+        L?: string
+        format?: Format
+    }
+    ```
+
+- ### UserStatsForGameOptions
+
+    ```ts
+    type UserStatsForGameOptions = PlayerAchievementsOptions;
+    ```
+
+- ### OwnedGamesOptions
+
+    ```ts
+    type OwnedGamesOptions = {
+        includeAppinfo?: boolean,
+        includePlayedFreeGames?: boolean,
+        format?: Format
+    };
+    ```
+
+- ### RecentlyPlayedGamesOptions
+
+    ```ts
+    type RecentlyPlayedGamesOptions = {
+        count?: number;
+        format?: Format
+    };
+    ```
+
+- ### Output
+
+    ```ts
+    type Output = Promise<string | JSON | undefined>;
+    ```
--- mod.ts
+++ mod.ts
@@ -1,32 +1,46 @@
 import { request } from "./request.ts";
 
-type RelationShip = "all" | "friend";
-type Options = {
+export type RelationShip = "all" | "friend";
+export type Format = "json" | "xml";
+export type BaseOptions = {
+    key?: string;
+    requestInit?: RequestInit;
+}
+export type FormatOption = { format?: Format };
+export type GetNewsForAppOptions = BaseOptions & {
+    count?: number,
+    maxlength?: number
+} & FormatOption;
+export type PlayerAchievementsOptions = BaseOptions & {
+    L?: string
+} & FormatOption
+export type UserStatsForGameOptions = PlayerAchievementsOptions;
+export type OwnedGamesOptions = BaseOptions & {
+    includeAppinfo?: boolean,
+    includePlayedFreeGames?: boolean
+} & FormatOption;
+export type RecentlyPlayedGamesOptions = BaseOptions & {
+    count?: number;
+} & FormatOption;
+type Options = GetNewsForAppOptions & UserStatsForGameOptions & OwnedGamesOptions & RecentlyPlayedGamesOptions & {
     addr?: string
     appid?: number;
     gameid?: number;
     steamids?: [string];
     steamid?: string;
     relationship?: RelationShip;
-    key?: string;
     lang?: string;
-    count?: number;
-    maxlength?: number;
-    includeAppinfo?: boolean;
-    includePlayedFreeGames?: boolean;
-    appidsFilter?: [number];
-    format?: string;
-    requestInit?: RequestInit;
 }
+
 type Output = Promise<string | JSON | undefined>
 
 const apiKeyError = new Error("You must add your steam api key in the instance or as options when calling this function!");
 export default class Steam {
-    b = "http://api.steampowered.com";
-    v = "v0002";
-    defaultkey?: string;
-    defaultRequestInit: RequestInit;
-    constructor (defaultkey?: string) {
+    private b = "http://api.steampowered.com";
+    private v = "v0002";
+    private defaultkey?: string;
+    private defaultRequestInit: RequestInit;
+    constructor(defaultkey?: string) {
         this.defaultkey = defaultkey;
         this.defaultRequestInit = {
             method: 'GET',
@@ -34,15 +48,15 @@ export default class Steam {
         };
     }
 
-    getRequestInit(options?: Options) {
+    private getRequestInit(options?: Options) {
         return (options && options.requestInit) ? options.requestInit : this.defaultRequestInit;
     }
 
-    getKey(options?: Options) {
+    private getKey(options?: Options) {
         return (options && options.key) ? options.key : this.defaultkey;
     }
 
-    searchParams(url: URL, options?: Options, protect = false): URL {
+    private searchParams(url: URL, options?: Options, protect = false): URL {
         if (protect) {
             const key = this.getKey(options);
             if (!key) throw apiKeyError;
@@ -57,7 +71,6 @@ export default class Steam {
             if (options.relationship) url.searchParams.append("relationship", options.relationship);
             if (options.includeAppinfo) url.searchParams.append("include_appinfo", options.includeAppinfo.toString());
             if (options.includePlayedFreeGames) url.searchParams.append("include_played_free_games", options.includePlayedFreeGames.toString());
-            if (options.appidsFilter) url.searchParams.append("appids_filter", options.appidsFilter.toString());
             if (options && options.lang) url.searchParams.append("L", options.lang);
             if (options.count) url.searchParams.append("count", options.count.toString());
             if (options.maxlength) url.searchParams.append("maxlength", options.maxlength.toString());
@@ -66,84 +79,170 @@ export default class Steam {
         return url;
     }
 
-    url(Interface: string, Method: string, Version: string = this.v): URL {
+    private url(Interface: string, Method: string, Version: string = this.v): URL {
         return new URL(`${this.b}/${Interface}/${Method}/${Version}`);
     }
 
-    async GetAppList(options?: Options): Output {
+    /**
+     * Get all steam applications data.
+     * @param {Format?} format
+     * @return {Output}
+     */
+    async GetAppList(format?: Format): Output {
+        const options: Options = { format: format }
         let url = this.url("ISteamApps", "GetAppList");
         url = this.searchParams(url, options);
         const result = await request(url.toString(), this.getRequestInit(options)).catch((error: Error) => { throw error; });
         return (result.unwrap().body) ? result.unwrap().body : void 0;
     }
 
-    async GetServersAtAddress(options?: Options): Output { // addr
+    /**
+     * Get all steam application data from a server.
+     * @param {string!} addr IP adress of target server.
+     * @param {Format?} format
+     * @return {Output}
+     */
+    async GetServersAtAddress(addr?: string, format?: Format): Output {
+        const options: Options = { addr: addr, format: format }
         let url = this.url("ISteamApps", "GetServersAtAddress", "v0001");
         url = this.searchParams(url, options);
         const result = await request(url.toString(), this.getRequestInit(options)).catch((error: Error) => { throw error; });
         return (result.unwrap().body) ? result.unwrap().body : void 0;
     }
 
-    async GetNewsForApp(options?: Options): Output { // appid, count, maxlength, format
+    /**
+     * Returns the latest news of a game specified by its appID.
+     * @param appid
+     * @param {GetNewsForAppOptions} options {
+     *      count?: number,         // How many news enties you want to get returned.
+     *      maxlength?: number,     // Maximum length of each news entry.
+     *      format?: Format
+     * }
+     * @return {Output}
+     */
+    async GetNewsForApp(appid: number, options?: GetNewsForAppOptions): Output {
+        const params: Options = { appid: appid, ...options };
         let url = this.url("ISteamNews", "GetNewsForApp");
-        url = this.searchParams(url, options);
+        url = this.searchParams(url, params);
         const result = await request(url.toString(), this.getRequestInit(options)).catch((error: Error) => { throw error; });
         return (result.unwrap().body) ? result.unwrap().body : void 0;
     }
 
-    async GetGlobalAchievementPercentagesForApp(options?: Options): Output { // gameid, format
+    /**
+     * Returns on global achievements overview of a specific game in percentages.
+     * @param {number!} gameid AppID of the game you want the news of.
+     * @param {Format?} format
+     * @return {Output}
+     */
+    async GetGlobalAchievementPercentagesForApp(gameid: number, format?: Format): Output {
+        const options: Options = { gameid: gameid, format: format };
         let url = this.url("ISteamUserStats", "GetGlobalAchievementPercentagesForApp");
         url = this.searchParams(url, options);
         const result = await request(url.toString(), this.getRequestInit(options)).catch((error: Error) => { throw error; });
         return (result.unwrap().body) ? result.unwrap().body : void 0;
     }
-
-    async GetPlayerSummaries(options?: Options): Output { // steamids, format
+    
+    /**
+     * Returns basic profile information for a list of 64-bit Steam IDs.
+     * @param {[string]!} steamids Comma-delimited list of 64 bit Steam IDs to return profile information for. Up to 100 Steam IDs can be requested.
+     * @param {Format?} format
+     * @return {Output}
+     */
+    async GetPlayerSummaries(steamids: [string], format?: Format): Output {
+        const options: Options = { steamids: steamids, format: format };
         let url = this.url("ISteamUser", "GetPlayerSummaries");
         url = this.searchParams(url, options, true);
         const result = await request(url.toString(), this.getRequestInit(options)).catch((error: Error) => { throw error; });
         return (result.unwrap().body) ? result.unwrap().body : void 0;
     }
 
-    async GetFriendList(options?: Options): Output { // steamid, relationship, format
+    /**
+     * Returns the friend list of any Steam user, provided his Steam Community profile visibility is set to "Public".
+     * @param {string!} steamid 64 bit Steam ID to return friend list for.
+     * @param {RelationShip!} relationship Relationship filter. Possibles values: all, friend.
+     * @param {Format?} format
+     * @return {Output}
+     */
+    async GetFriendList(steamid: string, relationship: RelationShip, format?: Format): Output {
+        const options: Options = { steamid: steamid, relationship: relationship, format: format };
         let url = this.url("ISteamUser", "GetFriendList", "v0001");
         url = this.searchParams(url, options, true);
         const result = await request(url.toString(), this.getRequestInit(options)).catch((error: Error) => { throw error; });
         return (result.unwrap().body) ? result.unwrap().body : void 0;
     }
 
-    async GetPlayerAchievements(options?: Options): Output { // steamid, appid, L
+    /**
+     * Returns a list of achievements for this user by app id.
+     * @param {string!} steamid 64 bit Steam ID to return friend list for.
+     * @param {number!} appid The ID for the game you're requesting.
+     * @param {Options} options {
+     *      L?: string,             // Language. If specified, it will return language data for the requested language.
+     *      format?: Format
+     * }
+     * @return {Output}
+     */
+    async GetPlayerAchievements(steamid: string, appid: number, options?: PlayerAchievementsOptions): Output {
+        const params: Options = { steamid: steamid, appid: appid, ...options }
         let url = this.url("ISteamUserStats", "GetPlayerAchievements", "v0001");
-        url = this.searchParams(url, options, true);
-        const result = await request(url.toString(), this.getRequestInit(options)).catch((error: Error) => { throw error; });
+        url = this.searchParams(url, params, true);
+        const result = await request(url.toString(), this.getRequestInit(params)).catch((error: Error) => { throw error; });
         return (result.unwrap().body) ? result.unwrap().body : void 0;
     }
 
-    async GetUserStatsForGame(options?: Options): Output { //steamid, appid, L
+    /**
+     * Returns a list of achievements for this user by app id.
+     * @param {string!} steamid 64 bit Steam ID to return friend list for.
+     * @param {number!} appid The ID for the game you're requesting.
+     * @param {UserStatsForGameOptions} options
+     * @return {Output}
+     */
+    async GetUserStatsForGame(steamid: string, appid: number, options?: UserStatsForGameOptions): Output {
+        const params: Options = { steamid: steamid, appid: appid, ...options }
         let url = this.url("ISteamUserStats", "GetUserStatsForGame");
-        url = this.searchParams(url, options, true);
-        const result = await request(url.toString(), this.getRequestInit(options)).catch((error: Error) => { throw error; });
+        url = this.searchParams(url, params, true);
+        const result = await request(url.toString(), this.getRequestInit(params)).catch((error: Error) => { throw error; });
         return (result.unwrap().body) ? result.unwrap().body : void 0;
     }
 
-    async GetNumberOfCurrentPlayers(options?: Options): Output { // appid
+    /**
+     * Returns number of current players by app id.
+     * @param {number!} appid The ID for the game you're requesting.
+     * @param {Format} format
+     * @return {Output}
+     */
+    async GetNumberOfCurrentPlayers(appid: number, format?: Format): Output {
+        const params: Options = { appid: appid, format: format };
         let url = this.url("ISteamUserStats", "GetNumberOfCurrentPlayers", "v0001");
-        url = this.searchParams(url, options);
-        const result = await request(url.toString(), this.getRequestInit(options)).catch((error: Error) => { throw error; });
+        url = this.searchParams(url, params);
+        const result = await request(url.toString(), this.getRequestInit(params)).catch((error: Error) => { throw error; });
         return (result.unwrap().body) ? result.unwrap().body : void 0;
     }
 
-    async GetOwnedGames(options?: Options): Output { // steamid, include_appinfo, include_played_free_games, format, appids_filter
+    /**
+     * Returns a list of games a player owns along with some playtime information.
+     * @param {string!} steamid 64 bit Steam ID to return friend list for.
+     * @param {OwnedGamesOptions} options
+     * @return {Output}
+     */
+    async GetOwnedGames(steamid: string, options?: OwnedGamesOptions): Output {
+        const params: Options = { steamid: steamid, ...options }
         let url = this.url("IPlayerService", "GetOwnedGames", "v0001");
-        url = this.searchParams(url, options, true);
-        const result = await request(url.toString(), this.getRequestInit(options)).catch((error: Error) => { throw error; });
+        url = this.searchParams(url, params, true);
+        const result = await request(url.toString(), this.getRequestInit(params)).catch((error: Error) => { throw error; });
         return (result.unwrap().body) ? result.unwrap().body : void 0;
     }
 
-    async GetRecentlyPlayedGames(options?: Options): Output { // steamid, count, format
+    /**
+     * Returns a list of games a player has played in the last two weeks.
+     * @param {string!} steamid 64 bit Steam ID to return friend list for.
+     * @param {RecentlyPlayedGamesOptions} options
+     * @return {Output}
+     */
+    async GetRecentlyPlayedGames(steamid: string, options?: RecentlyPlayedGamesOptions): Output { // steamid, count, format
+        const params: Options = { steamid: steamid, ...options };
         let url = this.url("IPlayerService", "GetRecentlyPlayedGames", "v0001");
-        url = this.searchParams(url, options, true);
-        const result = await request(url.toString(), this.getRequestInit(options)).catch((error: Error) => { throw error; });
+        url = this.searchParams(url, params, true);
+        const result = await request(url.toString(), this.getRequestInit(params)).catch((error: Error) => { throw error; });
         return (result.unwrap().body) ? result.unwrap().body : void 0;
     }
 }
